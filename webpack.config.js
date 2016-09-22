3626a6340e0e513dcf0fcb404e22e938d64523d8
Bridges, Jonathan D <Jonathan.Bridges@Mercy.Net>
2016-09-22 18:28:30 -0500

    fix scss to be loaded at index

--- webpack.config.js
+++ webpack.config.js
@@ -15,13 +15,13 @@ loaders.push({
 		'css'
 	]
 });
-// local scss modules
+// local scss
 loaders.push({
 	test: /[\/\\]src[\/\\].*\.scss/,
 	loaders: [
 		'style?sourceMap',
-		'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
-		'sass'
+		'css?sourceMap&-minimize',
+		'sass?sourceMap'
 	]
 });
 
